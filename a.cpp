a77cbef1a437fbcd075a401b6a5943c02d5c6f60
lyle <lixianfa.official@gmail.com>
2014-03-17 15:43:00 +0800

    add a.cpp

---
+++ a.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: a.cpp
+	> Author: Lyle
+	> Mail: lixianfayx@163.com 
+	> Created Time: 2014年03月17日 星期一 15时40分18秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	printf("hello world");
+}
